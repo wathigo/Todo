58c6d2826c6b83559322a331e160553341379973
wathigo <wathigosimon@gmail.com>
2022-05-31 16:12:53 +0300

    Fetch data from the database instead of using hardcoded one

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todo'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'Todo {self.id} {self.description}'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3'
-    }, {
-        'description': 'Todo 4'
-    }])
+    return render_template('index.html', data=Todo.query.all())
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=3000)
